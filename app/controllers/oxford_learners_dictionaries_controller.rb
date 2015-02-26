0e775f89fd62cf4df5f4510a0259f12e09661677
Felipe Gentil <cdigentil@gmail.com>
2015-02-25 21:01:28 -0300

    Already showing definitions after looking for a word

--- app/controllers/oxford_learners_dictionaries_controller.rb
+++ app/controllers/oxford_learners_dictionaries_controller.rb
@@ -1,7 +1,10 @@
 class OxfordLearnersDictionariesController < ApplicationController
-  def index
-  end
-
   def search
+    word = params[:oxford_learners_dictionaries][:word]
+    if word
+      @oxford = OxfordLearnersDictionaries::English.new(word)
+      @oxford.look_up
+    end
+    render :index
   end
 end
--- spec/controllers/oxford_learners_dictionaries_controller_spec.rb
+++ spec/controllers/oxford_learners_dictionaries_controller_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
 
 describe OxfordLearnersDictionariesController do
-  describe 'GET index' do
-  end
-
   describe 'GET search' do
+    context 'with word' do
+      it 'renders index with definitions' do
+        get :index, word: "car"
+        expect(OxfordLearnersDictionaries::English).to_not receive(:new)
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'without word' do
+      it 'renders index without @oxford' do
+        get :index, word: ""
+        expect(OxfordLearnersDictionaries::English).to_not receive(:new)
+        expect(response.status).to eq 200
+      end
+    end
   end
 end
