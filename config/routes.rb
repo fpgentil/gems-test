1ed457480365fa8b96736cc1e8f3514cdbbb3d76
Felipe Gentil <cdigentil@gmail.com>
2015-03-12 20:20:57 -0300

    Add a simple interface with a link to test OxfordEnglishDictionaries

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-    resources :oxford_learners_dictionaries, only: :index do
-      collection do
-        get :search
-      end
+  root 'home#index'
+
+  resources :oxford_learners_dictionaries, only: :index do
+    collection do
+      get :search
     end
+  end
 end
